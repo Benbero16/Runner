cb7483e6a87d0b7debaac115a270c967140b7957
Berat Yiğit Uğurlu <243005015@stu.mudanya.edu.tr>
2025-08-01 15:54:43 +0300

    2. taktik hareket için

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,53 +1,113 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
-    [SerializeField] Animator myAnim;
+    [SerializeField] public Animator myAnim;
     [SerializeField] public float speed;
-    [SerializeField] public float shift;
+    [SerializeField] public float shift = 2;
+    [SerializeField] public bool isLeft, isMiddle, isRight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        isMiddle = true;
+        // transform.position= new Vector3 (0, 0, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*speed*Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.A))
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && isLeft == false)
+
         {
+            if (isMiddle)
+            {
+                isLeft = true;
+                isMiddle = false;
+            }
+            else if (isRight)
+            {
+                isMiddle = true;
+                isRight = false;
+            }
             transform.Translate(new Vector3(-shift, 0, 0));
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+
+        else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && isRight == false)
+
         {
-            transform.Translate(shift, 0, 0);
+            if (isMiddle)
+            {
+                isRight = true;
+                isMiddle = false;
+
+            }
+            else if (isLeft)
+            {
+                isMiddle = true;
+                isLeft = false;
+            }
+            transform.Translate(new Vector3(shift, 0, 0));
         }
-       // rb.MovePosition(transform.position + Vector3.forward *  speed * Time.deltaTime);
-       // transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
+
+
+
+        // 1.yöntem
+        /* if (Input.GetKeyDown(KeyCode.A) && transform.position.x>-0.5f )
+         {
+             transform.Translate(new Vector3(-shift, 0, 0));
+         }
+         else if (Input.GetKeyDown(KeyCode.D) && transform.position.x<0.5f) 
+         {
+             transform.Translate(shift, 0, 0);
+
+         }
+        */
+
+
+        // rb.MovePosition(transform.position + Vector3.forward * speed * Time.deltaTime);
+
+
+
+        // transform translate ile ýþýnlama 
+        //  transform.Translate(Vector3.forward*speed*Time.deltaTime);
+
+
         /*  if (Input.GetKey(KeyCode.A))
           {
               myAnim.SetBool("Run", true);
-
           }
           else if (Input.GetKeyUp(KeyCode.A))
           {
-
               myAnim.SetBool("Run", false);
-
-
           }
-          if (Input.GetKeyDown(KeyCode.Space))
+          if (Input.GetKeyDown(KeyCode.Space)) 
           {
-
-              myAnim.SetBool("Jump" , true);
-
+              myAnim.SetBool("Jump", true);        
           }
           else if (Input.GetKeyUp(KeyCode.Space))
           {
-              myAnim.SetBool("Jump" , false);
+              myAnim.SetBool("Jump", false);
           }
-       */
+
+          */
+
+
+
+
+
+
     }
+
+    private void FixedUpdate()
+    {
+        // rb.MovePosition(transform.position + Vector3.forward * speed * Time.deltaTime);
+
+    }
+
 }
