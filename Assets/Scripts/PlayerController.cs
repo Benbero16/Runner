e53666a99c148d56f9b1346e891c5b316670e8bb
Berat Yiğit Uğurlu <243005015@stu.mudanya.edu.tr>
2025-08-04 10:20:08 +0300

    engel ekleme

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,73 +2,16 @@
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Elements")]
     [SerializeField] Rigidbody rb;
     [SerializeField] public Animator myAnim;
-    [SerializeField] public float speed;
-    [SerializeField] public float shift = 2;
+    [Tooltip("bu değişken oyuncunun hızını ifade eder.")] [SerializeField] public float speed;
+    [Tooltip("bu değişken karakterin sağa ve sola x*2 kadar hareketini sağlar ")] [SerializeField] public float shift = 2; 
     [SerializeField] public bool isLeft, isMiddle, isRight;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [HideInInspector] public string denemeforgizleme;
+    [System.NonSerialized] public string denemeforgizleme_2;
+    void notlar()
     {
-        isMiddle = true;
-        // transform.position= new Vector3 (0, 0, 5);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && isLeft == false)
-
-        {
-            if (isMiddle)
-            {
-                isLeft = true;
-                isMiddle = false;
-            }
-            else if (isRight)
-            {
-                isMiddle = true;
-                isRight = false;
-            }
-            transform.Translate(new Vector3(-shift, 0, 0));
-        }
-
-        else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && isRight == false)
-
-        {
-            if (isMiddle)
-            {
-                isRight = true;
-                isMiddle = false;
-
-            }
-            else if (isLeft)
-            {
-                isMiddle = true;
-                isLeft = false;
-            }
-            transform.Translate(new Vector3(shift, 0, 0));
-        }
-
-
-
-
-
-        // 1.yöntem
-        /* if (Input.GetKeyDown(KeyCode.A) && transform.position.x>-0.5f )
-         {
-             transform.Translate(new Vector3(-shift, 0, 0));
-         }
-         else if (Input.GetKeyDown(KeyCode.D) && transform.position.x<0.5f) 
-         {
-             transform.Translate(shift, 0, 0);
-
-         }
-        */
-
 
         // rb.MovePosition(transform.position + Vector3.forward * speed * Time.deltaTime);
 
@@ -98,10 +41,76 @@ public class PlayerController : MonoBehaviour
           */
 
 
+        /*   if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && isLeft == false)
+
+           {
+               if (isMiddle)
+               {
+                   isLeft = true;
+                   isMiddle = false;
+               }
+               else if (isRight)
+               {
+                   isMiddle = true;
+                   isRight = false;
+               }
+               transform.Translate(new Vector3(-shift, 0, 0));
+           }
+
+           else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && isRight == false)
+
+           {
+               if (isMiddle)
+               {
+                   isRight = true;
+                   isMiddle = false;
+
+               }
+               else if (isLeft)
+               {
+                   isMiddle = true;
+                   isLeft = false;
+               }
+               transform.Translate(new Vector3(shift, 0, 0));
+           }
+
+           */
+
+    }
+    void MoveCharacter()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        
 
 
 
+        // 1.yöntem
+        #region karakter sınırlama
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -0.5f)
+        {
+            transform.Translate(new Vector3(-shift, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 0.5f)
+        {
+            transform.Translate(shift, 0, 0);
 
+        }
+        #endregion
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isMiddle = true;
+        // transform.position= new Vector3 (0, 0, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+MoveCharacter();
     }
 
     private void FixedUpdate()
